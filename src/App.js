978a86a8b3e3460e47d6e3a223938a4cd277f9a2
Nate Nasteff <nnasteff@protonmail.com>
2020-09-25 01:29:08 -0400

    added textarea and button components, added some styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from './components/Header/Header.js';
 import CssInput from './components/CssInput/CssInput.js';
 import classes from './App.module.css';
+import Button from './components/Button/Button.js';
 
 
 function App() {
   return (
-    <div className={classes.App}>
-      <Header></Header>
+    <div className={classes.App}>  
+      <Header/>
+      <div className={classes.Lines}/>
       <CssInput submit="" />
+      <Button><center>MINIFY!</center></Button>
     </div>
   );
 }
--- src/components/CssInput/CssInput.js
+++ src/components/CssInput/CssInput.js
@@ -1,16 +1,23 @@
 import React from "react";
 import classes from "./CssInput.module.css";
+import Grid from '@material-ui/core/Grid';
 
 const CssInput = (props) => {
   return (
-    <div className={classes.MagInput}>
-      <form onSubmit={props.submit}>
-        <textarea type="text" value="Copy & Paste your CSS here!" onChange="" />
-        <br />
-        <br />
-      </form>
-    </div>
-  );
-};
+    <Grid container className={classes.CssInput} spacing={0}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={6}>
+          <Grid key={"textarea1"} item>
+            <form onSubmit={props.submit}>
+              <textarea placeholder="Copy and paste your CSS code here!"/>
+            </form>
+          </Grid>
+          <Grid key={"textarea2"} item>
+            <textarea placeholder="Minified CSS will appear here."/>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  )};
 
 export default CssInput;
